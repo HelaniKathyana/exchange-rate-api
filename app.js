6a455b80ff4cae0e1f615dfcad0a333d0ed0dca0
HelaniW <helani.waidyarathne@xitricon.com>
2023-04-24 23:44:42 +0530

    fixed cors issues

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors'); 
 const app = express();
 const port = 3000;
 
 // Import the API routes from api.js
 const apiRoutes = require('./api');
 const swaggerUi = require('./swagger');
+
+app.use(cors());
 
 // Mount the API routes at the /api endpoint
 app.use('/api', apiRoutes);
